4ebd0e5b48734148b6649736c16496b047fee3e2
Anudeep <anudeep.roy@liferay.com>
2025-07-15 17:56:21 +0530

    Code cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,5 @@
 import Head from "next/head";
 import Homepage from "./homepage";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
     <>
