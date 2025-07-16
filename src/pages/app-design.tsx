ffcb74343d9f75fbd115d6e5f650bb1623a0c40e
Anudeep <anudeep.roy@liferay.com>
2025-07-16 10:56:20 +0530

    App design page (Non-responsive)

--- src/pages/app-design.tsx
+++ src/pages/app-design.tsx
@@ -1,7 +1,63 @@
+import Header from "@/components/header"
+import FooterContainer from "@/components/footerContainer"
+import PageBanner from "@/components/pageBanner"
+import Template from "@/components/template"
+import Course from "@/components/course"
+
 export default function AppDesign() {
     return (
         <div className="app-design">
-            <h1>App Design</h1>
+            <div className="container">
+                <Header />
+                <PageBanner
+                    title={'app design'}
+                    subtitle={'Our mobile designs bring intuitive digital solutions to your customers right at their fingertips.'}
+                />
+                <div className="templates">
+                    <Template
+                        image={'/assets/app-design/desktop/image-airfilter.jpg'}
+                        title={'AIRFILTER'}
+                        description={"Solving the problem of poor indoor air quality by filtering the air"}
+                    />
+                    <Template
+                        image={'/assets/app-design/desktop/image-eyecam.jpg'}
+                        title={'EYECAM'}
+                        description={'Product that lets you edit your favorite photos and videos at any time'}
+                    />
+                    <Template
+                        image={'/assets/app-design/desktop/image-faceit.jpg'}
+                        title={'FACEIT'}
+                        description={'Get to meet your favorite internet superstar with the faceit app'}
+                    />
+                    <Template
+                        image={'/assets/app-design/desktop/image-todo.jpg'}
+                        title={'TODO'}
+                        description={'A todo app that features cloud sync with light and dark mode'}
+                    />
+                    <Template
+                        image={'/assets/app-design/desktop/image-loopstudios.jpg'}
+                        title={'LOOPSTUDIOS'}
+                        description={'A VR experience app made for Loopstudios'}
+                    />
+                </div>
+                <div className="home-courses page-courses">
+                    <div className="course-left">
+                        <Course
+                            image={'/assets/home/desktop/image-web-design-large.jpg'}
+                            title={'web Design'}
+                            link={'/web-design'}
+                        />
+                    </div>
+                    <div className="course-right">
+                        <Course
+                            image={'/assets/home/desktop/image-graphic-design.jpg'}
+                            title={'graphic Design'}
+                            link={'/graphic-design'}
+                        />
+                    </div>
+                </div>
+            </div>
+            <FooterContainer />
         </div>
     )
 }
